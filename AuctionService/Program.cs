8ebba4da3e7cc6a7fe5438c22ce118b936a43a0d
tamim <tamimarefinanik@gmail.com>
2024-02-04 23:38:03 +0600

    adding docker compose with postgres

--- AuctionService/Program.cs
+++ AuctionService/Program.cs
@@ -1,8 +1,19 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var str = builder.Configuration.GetConnectionString("DefaultConnection");
+Console.WriteLine("Conn string" + str);
+
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -15,3 +26,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+//https://gale.udemy.com/course/build-a-microservices-app-with-dotnet-and-nextjs-from-scratch/learn/lecture/
+// 11 
