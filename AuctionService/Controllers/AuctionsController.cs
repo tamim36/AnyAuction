c604f731b74c0baf3b18b84d18fb828d31bd1d88
tamim <tamimarefinanik@gmail.com>
2024-09-24 15:23:45 +0600

    adding editor config and get auctions methods

---
+++ AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,58 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions
+                .Include(auction => auction.Item)
+                .OrderBy(auction => auction.Item.Make)
+                .ToListAsync();
+
+            return _mapper.Map<List<AuctionDto>>(auctions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(auction => auction.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return _mapper.Map<AuctionDto>(auction);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
